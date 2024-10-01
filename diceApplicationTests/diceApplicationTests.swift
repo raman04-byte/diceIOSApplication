172d21764649b41a88e65ffbd178583e4d3052f5
Raman Tank <ramantank04022002@gmail.com>
2024-10-01 15:55:41 +0530

    Initial Commit

---
+++ diceApplicationTests/diceApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  diceApplicationTests.swift
+//  diceApplicationTests
+//
+//  Created by Raman Tank on 01/10/24.
+//
+
+import Testing
+@testable import diceApplication
+
+struct diceApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
